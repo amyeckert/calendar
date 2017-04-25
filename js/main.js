e0e8007739124bd46c1f0657b7df001f92d22c49
Amy Eckert <amy@amyeckertphoto.com>
2017-04-25 15:41:26 -0400

    wrote ajax calls into one step

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 
-var CAL = CAL || {}; //checks to see if this object already exists first, if not, create new object. If var CAL = 'foo'; existed, it would be overriden !
+// var CAL = CAL || {}; //checks to see if this object already exists first, if not, create new object. If var CAL = 'foo'; existed, it would be overriden !
+$(document).ready(function() {
 
-CAL.utilities = {
-	listen: function(){
+
+	var listen= function(){
 		// scrollTo functionality
 		$('.nav-main a').on('click', function(event) {
 			event.preventDefault();
@@ -13,78 +14,82 @@ CAL.utilities = {
 			//highlight current page in nav-main
 			var backToTop = $(this).addClass('is-current');
 		});
-	},
+	}
 
-	findEvents: function(){
-		console.log('checking for new events');
+	var findEvents = function(){
+		console.log('checking for next events');
+
 		var meetUpKey = '112f1c6d117217b646034221513f16';
+		var meetUpName = ['cherryhill-taichi-group', 'cherryhill-taichi-group'];
+
+		for (var i = meetUpName.length - 1; i >= 0; i--) {
+
+		 	var upcomingEvents = $.ajax({
+				type: 'GET',
+				url: 'https://api.meetup.com/' + meetUpName[i] + '/events?&sign=true&photo-host=public&page=20&fields=short_link&status=upcoming&_app_key=' + meetUpKey, 
+				dataType: 'jsonp'
+
+			}).done(function(response){
+					// for (var i = upcomingEvents.length - 1; i >= 0; i--) {
+						console.log(upcomingEvents);
+					// }
+
+
+				// for (var i = response.data.length - 1; i >= 0; i--) {
+				// 	var eventDateTime = response.data[i].time;
+				// 	var eventLink = response.data[i].short_link;
+				// 	var eventName = response.data[i].name;	
+				// }
+				
+		 		// var nextEvents = [eventName, eventLink, eventDateTime];
+		 		// console.log(nextEvents);
+			
+			}).fail(function(response){
+				console.log('error: ', response);
+			});
+		}
 
 		// Tai Chi Cherry Hill
-		var taiChi = 'cherryhill-taichi-group';
-	 	$.ajax({
-			type: 'GET',
-			url: 'https://api.meetup.com/' + taiChi + '/events?&sign=true&photo-host=public&page=20&fields=short_link,series&_app_key=' + meetUpKey, 
-			dataType: 'jsonp'
+		// var taiChi = 'cherryhill-taichi-group';
 
-		}).done(function(response){
+			// console.log(nextTaiChiEvent);		
 
-			for (var i = response.data.length - 1; i >= 0; i--) {
-			
-				var eventDateTime = response.data[i].time;
-				var eventLink = response.data[i].short_link;
-				var eventName = response.data[i].name;
-				
-				console.log(eventName, eventLink, eventDateTime);
-
-				console.log(response.data[0].description);
-			}
-		 
-
-		}).fail(function(response){
-			console.log('error: ', response);
-		});
 
 
 		//GDI-Camden
 		// var gdiCamden = 'Girl-Develop-It-Camden';
 		// $.ajax({
 		// 	type: 'GET',
-		// 	url: 'https://api.meetup.com/' + gdiCamden + '/events?&sign=true&photo-host=public&page=20&_app_key=' + meetUpKey, 
+		// 	url: 'https://api.meetup.com/' + gdiCamden + '/events?&sign=true&photo-host=public&page=4&fields=short_link&status=upcoming&_app_key=' + meetUpKey, 
 		// 	dataType: 'jsonp'
 
 		// }).done(function(response){
-		// 	console.log(response.data);
+
+		// 	for (var i = response.data.length - 1; i >= 0; i--) {
+			
+		// 		var eventDateTime = response.data[0].time;
+		// 		var eventLink = response.data[0].short_link;
+		// 		var eventName = response.data[0].name;
+		// 	}
+		// 	var nextGdiEvent = [eventName, eventLink, eventDateTime];
+		// 		console.log(nextGdiEvent);			
 
 		// }).fail(function(response){
 		// 	console.log('error: ', response);
 		// });
-	},
+	}
 
-	timeConverter: function(UNIX_timestamp){
-		  var a = new Date(UNIX_timestamp * 1000);
-		  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		  var year = a.getFullYear();
-		  var month = months[a.getMonth()];
-		  var date = a.getDate();
-		  var hour = a.getHours();
-		  var min = a.getMinutes();
-		  var sec = a.getSeconds();
-		  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
-		  return time;
-	},
-	addToCalendar: function(){
+	var convertDateTime = function(eventDateTime){
+		var eventDateTime = new Date(year, month, day, hours);
+		eventDateTime.toDateSTring();
+		console.log(eventDateTime);
+	}
+
+	var addToCalendar = function(){
 		// takes the date, time description and link to event's page on Meetup and creates a G-calendar event on my test G-calendar. 
 	}
 
-} // end CAL object
-
-
-$(document).ready(function() {
-	// console.log(CAL.utilities);
-
-	CAL.utilities.listen();
-	CAL.utilities.findEvents();
-	// CAL.utilities.timeConverter(eventDateTime);
-	// console.log(eventDateTime)
+	listen();
+	findEvents();
 
 });// END of doc.ready()
