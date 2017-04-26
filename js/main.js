c15793eb1e55c7d0da14dfde44f1c62689a39723
Amy Eckert <amy@amyeckertphoto.com>
2017-04-26 16:13:22 -0400

    digging into google calendar api

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
 				}
 				
 		 		var nextEvents = [eventName, eventLink, convertDateTime(eventDateTime)];
- 				console.log(nextEvents);
+ 				console.log(nextEvents, nextEvents[0]);
  				// return(nextEvents);
 						
 			}).fail(function(response){
@@ -44,6 +44,7 @@ $(document).ready(function() {
 			});
 		}
 	}
+
 	// source: https://stackoverflow.com/questions/847185/convert-a-unix-timestamp-to-time-in-javascript
 	var convertDateTime = function(eventDateTime){
 		var a = new Date(eventDateTime * 1000);
@@ -60,12 +61,145 @@ $(document).ready(function() {
 		console.log(time);
 	}
 
-	var addToCalendar = function(nextEvents){
-		// takes the date, time description and link to event's page on Meetup and creates a G-calendar event on my test G-calendar. 
+	// var addEventToCalendar = function(nextEvents) {
+	// 	// takes the date, time description and link to event's page on Meetup and creates a G-calendar event on my test G-calendar. 
+	// }
+
+	var createEvent = function(eventDateTime, eventLink, eventName){
+		
+		var event = {
+			'summary': ' Google I/O 2015',
+			// 'location': '800 Howard St., San Francisco, CA 94103',
+			'description': 'A chance to hear more about Google\'s developer products.',
+			'start': {
+			'dateTime': '2015-05-28T09:00:00-07:00',
+			'timeZone': 'America/Los_Angeles'
+			},
+			'end': {
+			'dateTime': '2015-05-28T17:00:00-07:00',
+			'timeZone': 'America/Los_Angeles'
+			}
+		};
+		var request = gapi.client.calendar.events.insert({
+			'calendarId': 'primary',
+			'resource': event
+		});
+
+		request.execute(function(event) {
+  			appendPre('Event created: ' + event.htmlLink);
+		});
+
+
+
+		$.ajax({
+			type: 'POST',
+			url: 'https://www.googleapis.com/calendar/v3/calendars/' + testCalendarId + '/events',
+			dataType: 'jsonp'
+
+		}).done(function(response){
+			console.log(response);
+
+		}).fail(function(response){
+				console.log('error: ', response);
+			});
 	}
+
+	//---------	TALK TO GOOGLE CALENDAR API----------------//
+
+	var gclientId = '419714143213-m78j61414b5h7u0uo91n40gidl5a0kce.apps.googleusercontent.com';
+	var gapiKey = 'AIzaSyA9QOKvd0OrCQwFDtkWD5TCYBj4nxm8ioI';
+	var gscopes = 'https://www.googleapis.com/auth/calendar';
+	var testCalendarId = 'amyeckertprojects.com_lrij2jrn2cub096ebeh6e16r94@group.calendar.google.com';
+
+
+
+
+//------------- LOAD RESOURCES / 0AUTH --------------//
+
+	function handleClientLoad() {
+        // Loads the client library and the auth2 library together for efficiency.
+        // Loading the auth2 library is optional here since `gapi.client.init` function will load
+        // it if not already loaded. Loading it upfront can save one network request.
+        gapi.load('client:auth2', initClient);
+        console.log('client auth2 library loaded');
+  	}
+
+  	function initClient() {
+        // Initialize the client with API key and People API, and initialize OAuth with an
+        // OAuth 2.0 client ID and scopes (space delimited string) to request access.
+    	gapi.client.init({
+            apiKey: 'AIzaSyA9QOKvd0OrCQwFDtkWD5TCYBj4nxm8ioI',
+            discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
+            clientId: '419714143213-dg82c6s6si9dgoe90po1tgdhpnj39hik.apps.googleusercontent.com',
+            scope: 'profile'
+        }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        });
+  	}
+
+  	function updateSigninStatus(isSignedIn) {
+        // When signin status changes, this function is called.
+        // If the signin status is changed to signedIn, we make an API call.
+        if (isSignedIn) {
+      		console.log('you are signed in');
+          	makePeopleApiCall();
+          	makeCalendarApiCall();
+        }
+  	}
+
+  	function handleSignInClick(event) {
+        // Ideally the button should only show up after gapi.client.init finishes, so that this
+        // handler won't be called before OAuth is initialized.
+        gapi.auth2.getAuthInstance().signIn();
+      	console.log('signed in successfully');
+  	}
+
+  	function handleSignOutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+        console.log('signed out successfully');
+  	}
+
+	//----------------- API CALLS ---------------------//
+
+  	function makePeopleApiCall() {
+        // Make an API call to the People API, and print the user's given name.
+        gapi.client.people.people.get({
+          	resourceName: 'people/me'
+        }).then(function(response) {
+          	console.log('Hello, SJPW webmaster: ' + response.result.names[0].givenName);
+        }, function(reason) {
+          	console.log('Error: ' + reason.result.error.message);
+        });
+
+        
+  	}
+  	function makeCalendarApiCall() {
+  		//get the calendar ID
+  		// gapi.client.calendarList.insert({
+  		// 	resourceName: 'primary'
+  		// }).then(function(response) {
+  		// 	console.log(response.result.calendarId);
+  		// }, function(reason) {
+  		// 	console.log('Error: ' + reason.result.error.message);
+        
+  		// });
+  	}
+
+	//----------------------- DO THIS STUFF -------------------//
 
 	listen();
 	findEvents();
+	handleClientLoad();
 	
+
+	// $('#authorize-button').on('click', function(e) {
+ //        // e.preventDefault();
+ //        handleSignInClick();
+ //        console.log('clicked the Log In button');
+ //    });
 
 });// END of doc.ready()
